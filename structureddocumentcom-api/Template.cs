88db03a8759792058d6cfeca7fcfa5eef82449e2
David Podhola <david.podhola@naseukoly.cz>
2013-09-18 10:56:49 +0200

    Initial Commit

---
+++ structureddocumentcom-api/Template.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.structureddocument.api
+{
+    public class Template
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CultureInfoName { get; set; }
+
+        public Template(dynamic t)
+        {
+            this.Id = t.Id;
+            this.Title = t.Title;
+            this.Description = t.Description;
+            this.CultureInfoName = t.Language().CultureInfoName;
+        }
+    }
+}
---
+++ structureddocumentcom-client/ITemplateService.cs
@@ -0,0 +1,17 @@
+﻿using com.structureddocument.api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.structureddocument.client
+{
+    public interface ITemplateService
+    {
+        Task<IEnumerable<Template>> GetAll();
+        Task Add(Template expense);
+        Task Delete(int id);
+        Task Update(Template expense);
+    }
+}
